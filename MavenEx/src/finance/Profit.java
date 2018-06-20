e7aa14f03ae7efac4b54c4bbd3d5001829213158
maverick <maverick@192.168.56.1>
2018-06-20 11:15:57 +0800

    cicd

---
+++ MavenEx/src/finance/Profit.java
@@ -0,0 +1,12 @@
+package finance;
+
+import java.math.BigInteger;
+
+public class Profit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		System.out.print("finance/profit");
+	}
+
+}
---
+++ MavenEx/src/operation/Kubernetes.java
@@ -0,0 +1,10 @@
+package operation;
+
+public class Kubernetes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("operation/kubernetes");
+	}
+
+}
